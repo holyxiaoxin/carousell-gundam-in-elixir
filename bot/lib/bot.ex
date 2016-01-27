629888d385d0e88f4d7d07ba945fa2f5496f140c
Wei Jie <leow_wei_jie@ida.gov.sg>
2016-01-27 21:16:42 +0800

    Add an update loop to process getUpdate API from telegram.

--- bot/lib/bot.ex
+++ bot/lib/bot.ex
@@ -1,6 +1,32 @@
 defmodule Bot do
+  alias Nadia.Model.Update
+  alias Nadia.Model.Message
+  alias Nadia.Model.Chat
+
   def main(_args) do
     IO.puts "starting...."
-    IO.inspect Nadia.get_updates(timeout: 8)
+    loop(0)
+  end
+
+  defp loop(update_id) do
+    Nadia.get_updates(timeout: 5, offset: update_id)
+    |> case do
+      {:ok, list} ->
+        if list == [] do
+          IO.puts "no updates"
+        else
+          %Update{update_id: update_id} = List.last(list)
+          %Update{message: %Message{text: text}} = List.first(list)
+          update_id = update_id + 1
+          process_message(%{text: text})
+        end
+      {:error, _} ->
+        IO.puts "error"
+    end
+    loop(update_id)
+  end
+
+  defp process_message(%{text: text}) do
+    IO.puts text
   end
 end
